4180c908712c813190512e417e688b206a5bed6e
James Wu on Server <minhsiang@cchntek.com>
2023-02-02 01:53:52 -0400

    first commit

---
+++ start.sh
@@ -0,0 +1,11 @@
+esdir=esdata
+
+mkdir -p ../${esdir}/es01
+mkdir -p ../${esdir}/es02
+mkdir -p ../${esdir}/es03
+mkdir -p ../${esdir}/es04
+
+chmod 777 ../${esdir}/es01
+chmod 777 ../${esdir}/es02
+chmod 777 ../${esdir}/es03
+chmod 777 ../${esdir}/es04
